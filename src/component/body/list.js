4980084bd6af88d6bf4b2d37c5c5b8e45cfd3d24
celine GA <celstuv@yahoo.fr>
2020-05-26 16:23:17 +0200

    1ere étape projet ToDoList React

---
+++ src/component/body/list.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function List() {
+  return (
+  <div>
+    <ul>
+    <li> Tache1 </li>
+    <li> Tache2 </li>
+    <li> Tache3 </li>
+    </ul>
+  </div>
+  );
+}
+
+
+
+export default List;
